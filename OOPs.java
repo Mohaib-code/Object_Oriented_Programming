1f098aacca0fafdeafeab2924b54a6d44da99eb4
Mohaib Molla <mohaibmolla1@gmail.com>
2026-04-05 20:20:14 +0530

    starting 2

--- OOPs.java
+++ OOPs.java
@@ -2,21 +2,30 @@ public class OOPs{
     public static void main(String args[]){
         Pen p1=new Pen();
         p1.tipSize(5);
-        System.out.println(p1.tip);
-        System.out.println(p1.color);
-        p1.color="Red";
-        System.out.println(p1.color);
+        System.out.println(p1.setTip());
+        System.out.println(p1.setColor());
+        p1.changeColor("Red");
+        System.out.println(p1.setColor());
 
     }
 }
 
 class Pen {
-    String color;
-    int tip;
-    void tipSize(int newTip){
-        tip=newTip;
+    private String color;
+    private int tip;
+    //Setter
+    String setColor(){
+        return this.color;
     }
+    int setTip(){
+        return this.tip;
+    }
+    //Getter
     void changeColor(String newColor){
         color=newColor;
     }
+    void tipSize(int newTip){
+        tip=newTip;
+    }
+    
 }
